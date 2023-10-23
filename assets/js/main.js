00015f2f5d6e024ca6c0b184b1fbab8c69a1a29c
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-10-24 02:02:26 +0900

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -263,11 +263,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const portfolioItems = document.querySelectorAll('.portfolio .col-lg-4.portfolio-item');
 
   portfolioItems.forEach((item) => {
-    item.addEventListener('click', function (event) {
-      // 이벤트 버블링을 중지하고 item 클릭 이벤트 실행
-      event.stopPropagation();
-      window.location.href = 'portfolio-details.html';
-    });
+    if (window.innerWidth >= 576) {
+      item.addEventListener('click', function (event) {
+        // 이벤트 버블링을 중지하고 item 클릭 이벤트 실행
+        event.stopPropagation();
+        window.location.href = 'portfolio-details.html';
+      });
+    }
 
     // bx-plus 아이콘 클릭 이벤트
     const plusIcons = item.querySelectorAll('.portfolio-info .preview-link');
@@ -278,6 +280,7 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
   });
+  
 });
 
 /*
